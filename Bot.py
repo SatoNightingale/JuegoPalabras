dddc59365ff34117d69c4123de1dccaf40ad05f0
SatoNightingale <davidmederosprieto@gmail.com>
2025-03-09 22:48:12 -0400

    Fix2

--- Bot.py
+++ Bot.py
@@ -196,10 +196,18 @@ async def inicializar_juego(bot: Bot, chat: Chat):
     # 'rondas': [],
     # 'game_running': False
 
-    juego['impostor'] = choice(juego['players'])
+    imp_id = choice(juego['players'])
+
+    juego['impostor'] = imp_id
+
+    # await user_imp = bot.get_chat_member(chat.id, juego['impostor'])
+    logger.info(f'El impostor es el id {juego['impostor']}')
+
     juego['vivos'] = juego['players'].copy()
 
     juego['palabra'] = elegir_palabra()
+
+    logger.info(f'La palabra es {juego['palabra']}')
 
     juego['jugadas'] = {}
     juego['rondas'] = []
